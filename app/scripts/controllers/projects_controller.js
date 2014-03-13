2b87d0221a23da0c2c554451ee62ce1d40d94d7c
Michał Świtoń <switonmichal@gmail.com>
2014-03-13 18:22:24 +0100

    Added actions to Modal - hope that it helps

--- app/scripts/controllers/projects_controller.js
+++ app/scripts/controllers/projects_controller.js
@@ -1,3 +1,11 @@
 Spreadsheet.ProjectsController = Ember.ObjectController.extend({
+    actions: {
+        doit: function () {
+            console.log('YEP!'); // @TODO
+        },
+        hideContent: function () {
+            $('#content-of-modal').hide(5000);
+        }
+    }
 });
 
